c2ab234f043223feedbd21ac8a1cd2647888aaef
FabFisk <fischettofabrizio@gmail.com>
2016-10-16 21:03:11 +0200

    Fix.

--- BibliotecaHibernate/test/dao/PrestitoDAOTest.java
+++ BibliotecaHibernate/test/dao/PrestitoDAOTest.java
@@ -1,58 +1,67 @@
-//package dao;
-//
-//import static org.junit.Assert.*;
-//
-//import org.junit.AfterClass;
-//import org.junit.BeforeClass;
-//import org.junit.Test;
-//
-//import model.Prestito;
-//
-//public class PrestitoDAOTest {
-//
-//	static PrestitoDAO pDAO;
-//	static Prestito p;
-//	
-//	@BeforeClass
-//	public static void SetUp() {
-//		pDAO = new PrestitoDAO();
-//		p = new Prestito(u, l, b, date, date);
-//		pDAO.createPrestito(p);
-//		assertNotNull(pDAO);
-//	}
-//	
-//	@Test
-//	public void createPrestito(){
-//		Prestito p = new Prestito(u, l, b, date, date);
-//		boolean token = pDAO.createPrestito(p);
-//		assertTrue(token);
-//	}
-//	
-//	@Test
-//	public void readPrestito(){
-//		Prestito p1 = pDAO.readPrestito(1);
-//		assertEquals(p.getId_prestito(), p1.getId_prestito());
-//	}
-//	
-//	@Test
-//	public void updatePrestito(){
-//		Prestito p1 = pDAO.readPrestito(1);
-//		p1.setDataScadenza(dataScadenza);
-//		boolean token = pDAO.updatePrestito(p1);
-//		assertTrue(token);	
-//	}
-//	
-//	@Test
-//	public void deletePrestito(){
-//		Prestito p1 = pDAO.readPrestito(2);
-//		boolean token = pDAO.deletePrestito(p1);
-//		assertTrue(token);		
-//	}
-//	
-//	@AfterClass
-//	public static void tearDown(){
-//		boolean token = pDAO.deletePrestito(p);
-//		assertTrue(token);
-//	}
-//
-//}
+package dao;
+
+import static org.junit.Assert.*;
+
+import java.util.Date;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import model.Biblioteca;
+import model.Libro;
+import model.Prestito;
+import model.Utente;
+
+public class PrestitoDAOTest {
+
+	static PrestitoDAO pDAO;
+	static Prestito p;
+	static Biblioteca b;
+	static Utente u;
+	static Libro l;
+	
+	@BeforeClass
+	public static void SetUp() {
+		pDAO = new PrestitoDAO();
+		p = new Prestito(u, l, b, new Date(), new Date());
+		pDAO.createPrestito(p);
+		b = new Biblioteca("Test");
+		u = new Utente("aaa", "aaa", "aa001");
+		l = new Libro("bbb", "bbb", "bb001", 2);
+		BibliotecaDAO bDAO = new BibliotecaDAO();
+		UtenteDAO uDAO = new UtenteDAO();
+		LibroDAO lDAO = new LibroDAO();
+		bDAO.createBiblioteca(b);
+		uDAO.createUtente(u);
+		lDAO.createLibro(l);
+		assertNotNull(pDAO);
+	}
+	
+	@Test
+	public void createPrestito(){
+		Prestito p = new Prestito(u, l, b, new Date(), new Date());
+		boolean token = pDAO.createPrestito(p);
+		assertTrue(token);
+	}
+	
+	@Test
+	public void readPrestito(){
+		Prestito p1 = pDAO.readPrestito(1);
+		assertEquals(p.getId_prestito(), p1.getId_prestito());
+	}
+	
+	@Test
+	public void deletePrestito(){
+		Prestito p1 = pDAO.readPrestito(5);
+		boolean token = pDAO.deletePrestito(p1);
+		assertTrue(token);		
+	}
+	
+	@AfterClass
+	public static void tearDown(){
+		boolean token = pDAO.deletePrestito(p);
+		assertTrue(token);
+	}
+
+}
